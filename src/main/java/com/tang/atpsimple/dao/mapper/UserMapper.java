212abeba05b1813cd3533380d3526be51919f127
tangky <945541696@qq.com>
2024-04-10 16:57:29 +0800

    insert user

--- src/main/java/com/tang/atpsimple/dao/mapper/UserMapper.java
+++ src/main/java/com/tang/atpsimple/dao/mapper/UserMapper.java
@@ -1,7 +1,15 @@
 package com.tang.atpsimple.dao.mapper;
 
 import com.tang.atpsimple.dao.po.User;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
-public interface UserMapper extends Mapper<User> {
+public interface UserMapper {
+
+    /**
+     * @param user
+     * @return
+     */
+    int insert(@Param("user") User user);
+
 }
